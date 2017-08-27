8afd009ed333c782e60650dca95182a9c23b224f
Matt Dean <matthewdean3d@gmail.com>
2017-08-27 20:58:57 +0100

    Character anim and further repo cleanup
    - Switch out PSDs for TIFF
    - Remove old MAX files

---
+++ Assets/Demo/Scripts/CameraOrbit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToonShading
+{
+    public class CameraOrbit : MonoBehaviour
+    {
+        public float orbitSpeed = 1f;
+
+        Vector2 mouseStartPos;
+
+        private void Update()
+        {
+            if(Input.GetButtonDown("Fire1"))
+            {
+                mouseStartPos = Input.mousePosition + new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if(Input.GetButton("Fire1"))
+            {
+                Vector3 mousePos = Input.mousePosition + new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
+                float rotation = ((mousePos.x - mouseStartPos.x) / Screen.width) * orbitSpeed;
+                Vector3 euler = new Vector3(transform.eulerAngles.x , transform.eulerAngles.y + rotation, transform.eulerAngles.z);
+                transform.eulerAngles = euler;
+            }
+        }
+    }
+}
