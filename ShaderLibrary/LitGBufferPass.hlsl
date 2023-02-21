2ebc275af9f164f076d7d9201fca6555f4ac100c
Kink3d <matthewdean3d@gmail.com>
2023-02-21 13:45:47 +0100

    Fix normals

--- ShaderLibrary/LitGBufferPass.hlsl
+++ ShaderLibrary/LitGBufferPass.hlsl
@@ -13,11 +13,13 @@ struct Attributes
     float4 positionOS   : POSITION;
     float3 normalOS     : NORMAL;
     float4 tangentOS    : TANGENT;
+    float2 texcoord     : TEXCOORD0;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct Varyings
 {
+    float2 uv                       : TEXCOORD0;
     half3 normalWS                  : TEXCOORD2;
 #if defined(REQUIRES_WORLD_SPACE_TANGENT_INTERPOLATOR)
     half4 tangentWS                 : TEXCOORD3;    // xyz: tangent, w: sign
@@ -39,6 +41,7 @@ Varyings LitGBufferPassVertex(Attributes input)
     VertexPositionInputs vertexInput = GetVertexPositionInputs(input.positionOS.xyz);
     VertexNormalInputs normalInput = GetVertexNormalInputs(input.normalOS, input.tangentOS);
 
+    output.uv = TRANSFORM_TEX(input.texcoord, _BaseMap);
     output.positionCS = vertexInput.positionCS;
     output.normalWS = normalInput.normalWS;
 
@@ -70,6 +73,7 @@ FragmentOutput LitGBufferPassFragment(Varyings input)
     UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
 
     #if defined(_NORMALMAP) || defined(_DETAIL)
+        float3 normalTS = SampleNormal(input.uv, TEXTURE2D_ARGS(_BumpMap, sampler_BumpMap));
         float sgn = input.tangentWS.w;      // should be either +1 or -1
         float3 bitangent = sgn * cross(input.normalWS.xyz, input.tangentWS.xyz);
         float3 normalWS = TransformTangentToWorld(normalTS, half3x3(input.tangentWS.xyz, bitangent.xyz, input.normalWS.xyz));
